a71a25728a06b64258ba2758feadb18bcc672acf
damien <damien@dam-lx>
2017-03-14 00:24:43 +0100

    initial commit of done folder + implementation of week01 until sector.c

---
+++ done/sector.c
@@ -0,0 +1,36 @@
+/*
+ * sector.c
+ *
+ *  Created on: Mar 13, 2017
+ *      Author: Damien & Léonard
+ */
+
+#include "sector.h"
+
+#include "error.h"
+#include "unixv6fs.h"
+
+/**
+ * @brief read one 512-byte sector from the virtual disk
+ * @param f open file of the virtual disk
+ * @param sector the location (in sector units, not bytes) within the virtual disk
+ * @param data a pointer to 512-bytes of memory (OUT)
+ * @return 0 on success; <0 on error
+ */
+int sector_read(FILE *f, uint32_t sector, void *data) {
+	M_REQUIRE_NON_NULL(f);
+	M_REQUIRE_NON_NULL(data);
+	if (0 == sector || sector > 63)
+		return ERR_BAD_PARAMETER;
+
+	if (fseek(f, sector * SECTOR_SIZE, SEEK_SET) != 0) {
+		return ERR_IO;
+	}
+
+	if (fread(&data, SECTOR_SIZE, f) != SECTOR_SIZE) {
+		return ERR_IO;
+	}
+
+	return 0;
+}
+
---
+++ done/test-inodes.c
@@ -0,0 +1,11 @@
+/*
+ * test-inodes.c
+ *
+ *  Created on: Mar 13, 2017
+ *      Author: damien
+ */
+
+int test(struct unix_filesystem *u){
+	return 0;
+}
+
