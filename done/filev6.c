2365b6d97c02552dc2da1fb72ca12f0bf1cbfe48
leolepro <leonard.berney@epfl.ch>
2017-03-21 21:36:37 +0100

    fix bugs in filev6 and sha and update makefile

--- done/filev6.c
+++ done/filev6.c
@@ -2,6 +2,7 @@
 #include "filev6.h"
 #include "error.h"
 #include "inode.h"
+#include "sector.h"
 
 int filev6_open(const struct unix_filesystem *u, uint16_t inr, struct filev6 *fv6) {
     M_REQUIRE_NON_NULL(fv6);
@@ -9,7 +10,7 @@ int filev6_open(const struct unix_filesystem *u, uint16_t inr, struct filev6 *fv
 
     fv6->u = u;
     fv6->i_number = inr;
-    int error = inode_read(u, inr, fv6->i_node);
+    int error = inode_read(u, inr, &fv6->i_node);
     fv6->offset = 0;
 
     return error;
@@ -19,20 +20,20 @@ int filev6_readblock(struct filev6 *fv6, void *buf) {
     M_REQUIRE_NON_NULL(fv6);
     M_REQUIRE_NON_NULL(buf);
 
-    if (fv6->offset >= inode_getsize(fv6->i_node)) {
+    if (fv6->offset >= inode_getsize(&fv6->i_node)) {
         return 0;
     }
 
-    int sector = inode_findsector(fv6->u, fv6->i_node, fv6->offset);
+    int sector = inode_findsector(fv6->u, &fv6->i_node, fv6->offset);
     int error = sector_read(fv6->u->f, sector, buf);
 
-    int last_sector_size = inode_getsize(fv6->i_node) % SECTOR_SIZE;
+    int last_sector_size = inode_getsize(&fv6->i_node) % SECTOR_SIZE;
     if (last_sector_size == 0) {
         last_sector_size = SECTOR_SIZE;
     }
 
     int read;
-    if (fv6->offset == inode_getsize(fv6->i_node) - last_sector_size) {
+    if (fv6->offset == inode_getsize(&fv6->i_node) - last_sector_size) {
         read = last_sector_size;
     } else {
         read = SECTOR_SIZE;
--- done/sha.c
+++ done/sha.c
@@ -2,11 +2,9 @@
 #include "sha.h"
 #include "error.h"
 #include "filev6.h"
+#include "inode.h"
 
 void compute_sha_from_content(const unsigned char* content, size_t n, unsigned char* hash) {
-    M_REQUIRE_NON_NULL(hash);
-    M_REQUIRE_NON_NULL(content);
-
     SHA256_CTX sha;
     SHA256_Init(&sha);
     SHA256_Update(&sha, content, n);
@@ -24,21 +22,17 @@ void print_sha(const unsigned char* sha) {
 }
 
 void print_sha_from_content(const unsigned char *content, size_t length) {
-    M_REQUIRE_NON_NULL(content);
-
-    unsigned char hash[SHA256_DIGEST_LENGTH];
+     unsigned char hash[SHA256_DIGEST_LENGTH];
     compute_sha_from_content(content, length, hash);
 
     print_sha(hash);
 }
 
 void print_sha_inode(struct unix_filesystem *u, struct inode inode, int inr) {
-    M_REQUIRE_NON_NULL(u);
-
-    if (!(inode->i_mode & IALLOC)) {
+    if (!(inode.i_mode & IALLOC)) {
         return;
     } else {
-        if (inode->i_mode & IFDIR) {
+        if (inode.i_mode & IFDIR) {
             puts("no SHA for directories.");
             return;
         } else {
@@ -51,8 +45,8 @@ void print_sha_inode(struct unix_filesystem *u, struct inode inode, int inr) {
 
     int code;
     int i = 0;
-    const unsigned char content[inode_getsize(&inode)];
-    while ((code = filev6_readblock(fv6, content + i * SECTOR_SIZE)) > 0) {
+    unsigned char content[inode_getsize(&inode)];
+    while ((code = filev6_readblock(&fv6, content + i * SECTOR_SIZE)) > 0) {
         i++;
     }
 
