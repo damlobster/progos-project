6fe9de593e6e444eaa3d7dc6a5c4480a5b7f27a0
Damien Martin <damien.martin@epfl.ch>
2017-03-22 12:23:30 +0100

    tests implemented

--- done/test-file.c
+++ done/test-file.c
@@ -13,6 +13,7 @@
 #include "mount.h"
 #include "sector.h"
 #include "unixv6fs.h"
+#include "sha.h"
 
 int test(const struct unix_filesystem *u) {
 
@@ -20,19 +21,38 @@ int test(const struct unix_filesystem *u) {
 	memset(&fs, 255, sizeof(fs));
 
 	struct inode in;
-	int err = inode_read(u, 3, &in);
-	if (err != 0) {
-		puts("filev6 open failed for inode #3");
-	}
-	inode_print(&in);
 
-	if ((in.i_mode & IFDIR) != 0) {
-		puts("which is a directory.");
-	} else {
-		char data[SECTOR_SIZE + 1];
-		memset(data, 0, sizeof(data));
-		sector_read(u->f, 0, data);
-		printf("%s", data);
+	for (int i_number = 3; i_number <= 5; i_number = i_number + 2) {
+		putchar('\n');
+
+		int err = inode_read(u, i_number, &in);
+		if (err != 0) {
+			printf("filev6 open failed for inode #%d.\n", i_number);
+		} else {
+			printf("Printing inode #%d:\n", i_number);
+			inode_print(&in);
+
+			if ((in.i_mode & IFDIR) != 0) {
+				puts("which is a directory.");
+			} else {
+				puts("the first sector of data which contains:");
+				char data[SECTOR_SIZE + 1];
+				data[SECTOR_SIZE] = '\0';
+				//memset(data, 0, sizeof(data));
+				int sector = inode_findsector(u, &in, 0);
+				sector_read(u->f, sector, data);
+				printf("%s\n", data);
+				puts("----");
+			}
+		}
+	}
+
+	puts("\nListing inodes SHA:");
+	puts("FIXME");
+	for(int i = 0; i<=u->s.s_isize; i++){
+		inode_read(u, i, &in);
+		//FIXME print_sha_inode(u, in, i);
+		putchar('\n');
 	}
 
 	return 0;
