3ffd3816b0a80eaac61ae61cd77b600352d021a2
damlobster <damlobster@gmail.com>
2017-04-05 09:00:28 +0200

    shell.c

---
+++ done/shell.c
@@ -0,0 +1,19 @@
+/*
+ * shell.c
+ *
+ * Created on: Apr 5, 2017
+ * Author: Léonard & Damien
+ */
+#include "mount.h"
+
+typedef int (*shell_fct)(const char** argc);
+
+struct shell_map {
+    const char* name;    // nom de la commande
+    shell_fct fct;      // fonction réalisant la commande
+    const char* help;   // description de la commande
+    size_t argc;        // nombre d'arguments de la commande
+    const char* args;   // description des arguments de la commande
+};
+
+struct unix_filesystem ufs;
