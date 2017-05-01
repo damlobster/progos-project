388ce8b18a1f20e18c965aa80066d23997fa0598
Damien Martin <damien.martin@epfl.ch>
2017-05-01 12:09:02 +0200

    first modifs to fs.c

--- done/fs.c
+++ done/fs.c
@@ -16,10 +16,10 @@
 #include <errno.h>
 #include <fcntl.h>
 
-static const char *hello_str = "Hello World!\n";
-static const char *hello_path = "/hello";
+static const char *fs_str = "Hello World!\n";
+static const char *fs_path = "/hello";
 
-static int hello_getattr(const char *path, struct stat *stbuf)
+static int fs_getattr(const char *path, struct stat *stbuf)
 {
 	int res = 0;
 
@@ -27,17 +27,17 @@ static int hello_getattr(const char *path, struct stat *stbuf)
 	if (strcmp(path, "/") == 0) {
 		stbuf->st_mode = S_IFDIR | 0755;
 		stbuf->st_nlink = 2;
-	} else if (strcmp(path, hello_path) == 0) {
+	} else if (strcmp(path, fs_path) == 0) {
 		stbuf->st_mode = S_IFREG | 0444;
 		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen(hello_str);
+		stbuf->st_size = strlen(fs_str);
 	} else
 		res = -ENOENT;
 
 	return res;
 }
 
-static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
+static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 			 off_t offset, struct fuse_file_info *fi)
 {
 	(void) offset;
@@ -48,49 +48,37 @@ static int hello_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
 
 	filler(buf, ".", NULL, 0);
 	filler(buf, "..", NULL, 0);
-	filler(buf, hello_path + 1, NULL, 0);
+	filler(buf, fs_path + 1, NULL, 0);
 
 	return 0;
 }
 
-static int hello_open(const char *path, struct fuse_file_info *fi)
-{
-	if (strcmp(path, hello_path) != 0)
-		return -ENOENT;
-
-	if ((fi->flags & 3) != O_RDONLY)
-		return -EACCES;
-
-	return 0;
-}
-
-static int hello_read(const char *path, char *buf, size_t size, off_t offset,
+static int fs_read(const char *path, char *buf, size_t size, off_t offset,
 		      struct fuse_file_info *fi)
 {
 	size_t len;
 	(void) fi;
-	if(strcmp(path, hello_path) != 0)
+	if(strcmp(path, fs_path) != 0)
 		return -ENOENT;
 
-	len = strlen(hello_str);
+	len = strlen(fs_str);
 	if (offset < len) {
 		if (offset + size > len)
 			size = len - offset;
-		memcpy(buf, hello_str + offset, size);
+		memcpy(buf, fs_str + offset, size);
 	} else
 		size = 0;
 
 	return size;
 }
 
-static struct fuse_operations hello_oper = {
-	.getattr	= hello_getattr,
-	.readdir	= hello_readdir,
-	.open		= hello_open,
-	.read		= hello_read,
+static struct fuse_operations available_ops = {
+	.getattr	= fs_getattr,
+	.readdir	= fs_readdir,
+	.read		= fs_read,
 };
 
 int main(int argc, char *argv[])
 {
-	return fuse_main(argc, argv, &hello_oper, NULL);
+	return fuse_main(argc, argv, &available_ops, NULL);
 }
