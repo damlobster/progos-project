8e8665f9f2af70b8d29624eb6d876e10e6d1a47c
Damien Martin <damien.martin@epfl.ch>
2017-05-01 12:14:55 +0200

    firsts modifs to fs.c

--- done/fs.c
+++ done/fs.c
@@ -1,12 +1,12 @@
 /*
-  FUSE: Filesystem in Userspace
-  Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
+ FUSE: Filesystem in Userspace
+ Copyright (C) 2001-2007  Miklos Szeredi <miklos@szeredi.hu>
 
-  This program can be distributed under the terms of the GNU GPL.
-  See the file COPYING.
+ This program can be distributed under the terms of the GNU GPL.
+ See the file COPYING.
 
-  gcc -Wall hello.c `pkg-config fuse --cflags --libs` -o hello
-*/
+ gcc -Wall hello.c `pkg-config fuse --cflags --libs` -o hello
+ */
 
 #define FUSE_USE_VERSION 26
 
@@ -16,69 +16,41 @@
 #include <errno.h>
 #include <fcntl.h>
 
-static const char *fs_str = "Hello World!\n";
-static const char *fs_path = "/hello";
+static int fs_getattr(const char *path, struct stat *stbuf) {
+    int res = 0;
 
-static int fs_getattr(const char *path, struct stat *stbuf)
-{
-	int res = 0;
+    memset(stbuf, 0, sizeof(struct stat));
 
-	memset(stbuf, 0, sizeof(struct stat));
-	if (strcmp(path, "/") == 0) {
-		stbuf->st_mode = S_IFDIR | 0755;
-		stbuf->st_nlink = 2;
-	} else if (strcmp(path, fs_path) == 0) {
-		stbuf->st_mode = S_IFREG | 0444;
-		stbuf->st_nlink = 1;
-		stbuf->st_size = strlen(fs_str);
-	} else
-		res = -ENOENT;
-
-	return res;
+    return res;
 }
 
 static int fs_readdir(const char *path, void *buf, fuse_fill_dir_t filler,
-			 off_t offset, struct fuse_file_info *fi)
-{
-	(void) offset;
-	(void) fi;
+        off_t offset, struct fuse_file_info *fi) {
+    (void) offset;
+    (void) fi;
 
-	if (strcmp(path, "/") != 0)
-		return -ENOENT;
+    filler(buf, ".", NULL, 0);
+    filler(buf, "..", NULL, 0);
 
-	filler(buf, ".", NULL, 0);
-	filler(buf, "..", NULL, 0);
-	filler(buf, fs_path + 1, NULL, 0);
-
-	return 0;
+    return 0;
 }
 
 static int fs_read(const char *path, char *buf, size_t size, off_t offset,
-		      struct fuse_file_info *fi)
-{
-	size_t len;
-	(void) fi;
-	if(strcmp(path, fs_path) != 0)
-		return -ENOENT;
-
-	len = strlen(fs_str);
-	if (offset < len) {
-		if (offset + size > len)
-			size = len - offset;
-		memcpy(buf, fs_str + offset, size);
-	} else
-		size = 0;
-
-	return size;
+        struct fuse_file_info *fi) {
+    size_t len;
+    (void) fi;
+    return 0;
 }
 
-static struct fuse_operations available_ops = {
-	.getattr	= fs_getattr,
-	.readdir	= fs_readdir,
-	.read		= fs_read,
-};
+static struct fuse_operations available_ops = { .getattr = fs_getattr,
+        .readdir = fs_readdir, .read = fs_read, };
 
-int main(int argc, char *argv[])
-{
-	return fuse_main(argc, argv, &available_ops, NULL);
+int main(int argc, char *argv[]) {
+    struct fuse_args args = FUSE_ARGS_INIT(argc, argv);
+    int ret = fuse_opt_parse(&args, NULL, NULL, arg_parse);
+    if (ret == 0) {
+        ret = fuse_main(args.argc, args.argv, &available_ops, NULL);
+        (void) umountv6(&fs);
+    }
+    return ret;
 }
