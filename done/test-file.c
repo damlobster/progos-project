fd3cf65be8d7857a36d6a5cca7ea5eba99da27fb
Damien Martin <damien.martin@epfl.ch>
2017-06-03 14:46:47 +0200

    use filev6 functions instead of inode_

--- done/test-file.c
+++ done/test-file.c
@@ -26,30 +26,29 @@
 int test(const struct unix_filesystem *u) {
     M_REQUIRE_NON_NULL(u);
 
-    struct filev6 fs;
-    memset(&fs, 255, sizeof(fs));
-
-    struct inode in;
+    struct filev6 fv6;
+    memset(&fv6, 255, sizeof(fv6));
 
     // print inode 3 and 5
     for (int i_number = 3; i_number <= 5; i_number += 2) {
         putchar('\n');
 
-        int err = inode_read(u, (uint16_t)i_number, &in);
+        int err = filev6_open(u, (uint16_t)i_number, &fv6);
         if (err != 0) {
             printf("filev6 open failed for inode #%d.\n", i_number);
         } else {
             printf("Printing inode #%d:\n", i_number);
-            inode_print(&in);
+            struct inode* inode = &fv6.i_node;
+            inode_print(inode);
 
-            if ((in.i_mode & IFDIR) != 0) {
+            if ((inode->i_mode & IFDIR) != 0) {
                 puts("which is a directory.");
             } else {
                 puts("the first sector of data which contains:");
                 char data[SECTOR_SIZE + 1];
                 data[SECTOR_SIZE] = '\0';
 
-                int sector = inode_findsector(u, &in, 0);
+                int sector = inode_findsector(u, inode, 0);
                 if(sector<0) return sector;
 
                 sector_read(u->f, (uint32_t)sector, data);
@@ -59,6 +58,7 @@ int test(const struct unix_filesystem *u) {
         }
     }
 
+    struct inode in;
     puts("\nListing inodes SHA:");
     for (uint16_t i = 1; i <= u->s.s_isize * INODES_PER_SECTOR; ++i) {
         if (inode_read(u, i, &in) == 0) print_sha_inode(u, in, i);
